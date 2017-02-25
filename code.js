24c11d868698cba5fc017861b6424305830f8b4a
Blair Erickson <blair@jamwix.com>
2017-02-25 11:38:43 -0500

    Page search functions

--- code.js
+++ code.js
@@ -1,13 +1,37 @@
 console.log("test code");
 
 
-$("runSearch").on("click", function() 
-{
-    	console.log("searched...");
-   });
+$( "#runSearch" ).click(function() {
+  console.log( "Handler for .click() called." );
+  getData();
+});
 
 
 function getData()
 {
-	console.log("searched");
+	console.log("searched function");
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "5ffb6633b0d147acb2fc12629521d06e",
+  'q': "Clinton"
+});
+
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+    console.log(result.response.docs[0]);
+
+}).fail(function(err) {
+  throw err;
+});
+
+
+
+
+
 }
